b3cd3e715b4e9785dbb903aa273d2562c7635dcf
Tina Chu49 <TinaChu49@gmail.com>
2019-04-29 15:17:12 +1200

    Yay finished it

--- Graduate-project-2019-01/Graduate.Test/Program.cs
+++ Graduate-project-2019-01/Graduate.Test/Program.cs
@@ -20,7 +20,40 @@ namespace ConsoleApp1
             if (string.IsNullOrEmpty(input))
                 return input;
 
-            return "";
+            char[] iChars = input.ToCharArray();
+            Array.Sort(iChars);
+
+            string processStr = new string(iChars);
+
+            string upperCase = "", lowerCase = "", even = "", odd = "";
+
+            for (int x = 0; x < processStr.Length; x++)
+            {
+                if (processStr[x] - 'z' <= 0 && processStr[x] - 'a' >= 0)
+                {
+                    lowerCase += processStr[x] + "";
+                }
+                else if (processStr[x] - 'Z' <= 0 && processStr[x] - 'A' >= 0)
+                {
+                    upperCase += processStr[x] + "";
+                }
+                else if (processStr[x] - '9' <= 0 && processStr[x] - '0' >= 0)
+                {
+                    int num = 0;
+                    int.TryParse(processStr[x] + "", out num);
+
+                    if (num % 2 == 0)
+                    {
+                        even += processStr[x];
+                    }
+                    else
+                    {
+                        odd += processStr[x];
+                    }
+                }
+            }
+
+            return lowerCase + upperCase + even + odd;
         }
     }
 }
