b1b4828836a7e0230dd1da5a789458de973a125b
Mark Barkman <Mark.Barkman@iag.co.nz>
2019-04-29 14:37:15 +1200

    Initial grad project git import

---
+++ Graduate-project-2019/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 12091029");
+            Console.WriteLine(alphaNumericSort("Hello World! 12091029"));
+            Console.WriteLine(alphaNumericSort("Sorting0123456789"));             // ginortS0246813579
+            Console.WriteLine(alphaNumericSort("foobar1237348421"));              // abfoor2244811337
+            Console.WriteLine(alphaNumericSort("789765445whjdbjwhwfbs977865"));   // bbdfhhjjswww446688555777799
+
+            Console.ReadKey();
+        }
+
+        public static string alphaNumericSort(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return "";
+        }
+    }
+}
